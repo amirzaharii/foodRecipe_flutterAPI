5c749e75fb7b8dd608ef544f8ccefcc75b5db128
Amir <amir.zahari@besquare.com.my>
2022-08-12 09:22:45 +0800

    update

--- lib/states/bookmarks.dart
+++ lib/states/bookmarks.dart
@@ -4,18 +4,17 @@ import 'package:food_recipe_project/models/foodRecipe_models.dart';
 class BookmarkCubit extends Cubit<Set<FoodRecipe>> {
   BookmarkCubit() : super({});
   void addIntoBookmark(FoodRecipe e, int index) {
-    emit({...state, e});
+    emit({
+      ...state,
+      e,
+    });
+    print(e.hits[index].recipe.label);
+    print(state);
+    print(index);
   }
 
   void removeFromBookmark(FoodRecipe e, int index) {
     state.remove(e);
     emit({...state});
   }
-
-  // void showFavourite(FoodRecipe e, int index) {
-  //   emit(Favourites(
-  //     foodRecipe: state,
-  //     index: index,
-  //   ));
-  // }
 }
